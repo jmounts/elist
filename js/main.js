8c76380163fb3642a2815eb137314be6afedc594
jmounts <mountzer@gmail.com>
2014-12-22 13:53:45 -0600

    input function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
-	$('.item').mouseenter(function() {
+
+////Hover Elements////    
+	$('.list').mouseenter(function() {
 		$('.check').show();
+        $('.delete').show();
+        $('.uncheck').show();
 	})
 	.mouseleave(function() {
 		$('.check').hide();
+        $('.delete').hide();
+        $('.uncheck').hide();
 	})
+
+////Input Items////
+    $('.add-b').click(function() {
+        var enter = $('#input').val();
+        if (enter.length > 0) {
+            $('<li/>').appendTo('.list').html($('#input').val());
+        }
+        else
+        {
+            alert("Forget Something?");
+        }    
+    });
+
+    $('#input').keydown(function(event) {
+        if (event.keyCode==13){
+            $('.add-b').trigger('click');
+        }
+    });
 });
