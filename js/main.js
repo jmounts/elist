6502c9fe0e0d538c2bfc4e5779b45a5a9a3799d3
jmounts <mountzer@gmail.com>
2014-12-24 10:51:44 -0600

    input working
    whoop whoop

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
 $(document).ready(function() {
 
-////Hover Elements////    
+    var submissions = 0;
+    var checkMark = '<button class="check small-1 columns">☑</button>'
+    var remove = '<button class="delete small-1 columns">×</button>' 
+    var marked = '<li class="row medium-6 small-12 small-centered columns item-marked">'
 
 ////Enter Function////
-    $('#input').keydown(function(event) {
-        if (event.keyCode==13) {
+    $('#input').keydown(function (enter) {
+        if (enter.keyCode==13) {
             $('.add-b').click();
         }
     });
 
 ////Input Items////
-    $('.add-b').click(function() {
-        var enter = $('#input').val();
-        if (enter.length > 0) {
-            $('.item').prepend("</li>").html($('#input').val());
-            $('.item').show();
-            $('.delete').show();
-        }
-        else
-        {
-            alert("Forget Something?");
-        }    
-    });
+    function getItem() {
+        $('#input').keydown(function (enter) {
+            if (enter.keyCode == 13) {
+                postItem();
+            }
+        });
+    }
+
+    getItem();
+
+    function postItem() {
+        var item = $('#input').val();
+        var work = '<li class="row medium-6 small-12 small-centered columns item">'+ checkMark + item + remove + '</li>';
+        $('#test').prepend(work);
+        $('#input').val('');
+        $('#test p:first-child')
+    }
 
 /////Delete////
     $(document).on("click", ".delete", function(){
