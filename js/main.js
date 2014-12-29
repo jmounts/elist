aa6ca19ddf5d49933dde5ef2e43da9a486141850
jmounts <mountzer@gmail.com>
2014-12-29 13:13:09 -0600

    alert

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         } 
         else 
         {
-            alert("Forget something?");
+            alert("Forget something? ..perhaps the apples?");
             }
         });
     }
